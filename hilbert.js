96cc89811190962e7968cdf9b89749c44038f711
Zachary Zimmerman <Oneman2feet@gmail.com>
2016-04-07 14:22:28 -0400

    we don't need video

--- hilbert.js
+++ hilbert.js
@@ -1,25 +1,10 @@
-// for logging
-function fire(e, data) {    
-  log.innerHTML += "\n" + e + " " + (data || '');
-}
-// globals
-var audio_context;
-var volume;
-// one-off initialization
-(function init(g){
-  try {
-    audio_context = new (g.AudioContext || g.webkitAudioContext);
-    fire('Audio context OK');
-    // shim
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
-    fire('navigator.getUserMedia ' + (navigator.getUserMedia ? 'OK' : 'fail'));
-    // use
-    navigator.getUserMedia(
-      {audio:true},
-      iCanHazUserMedia, 
-      function(e){fire('No live audio input ' + e);}
-    );
-  } catch (e) {
-    alert('No web audio support in this browser');
-  }
-}(window));
+if (navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia)) {
+  console.log('we have user media');
+  navigator.getUserMedia({audio:true}, function(stream) {
+    console.log("we have an audio stream!");
+  }, function(error) {
+    console.log("Capture error: ", error.code);
+  });
+} else {
+  console.log('you need a better browser');
+};
