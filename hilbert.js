e9410ecdf7ae0725dc8e0c7d4a95d659dbb30acc
Zachary Zimmerman <Oneman2feet@gmail.com>
2016-04-07 14:30:09 -0400

    phasing in more code

--- hilbert.js
+++ hilbert.js
@@ -1,5 +1,26 @@
-var AudioContext = window.AudioContext || window.webkitAudioContext;
-var audioCtx = new AudioContext();
+var AudioContext = (window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.oAudioContext || window.msAudioContext);
+
+if (AudioContext){
+  var audioCtx = new AudioContext();
+  var gainNode = audioCtx.createGain();
+  var analyser = audioCtx.createAnalyser();
+  //confusing, gain on oscilloscope, different for gain affecting input
+  // gainNode.gain.value = ui.gain.value;
+  gainNode.gain.value = 3;
+  analyser.smoothingTimeConstant = .9;
+  // analyser.fftSize = 512;
+  // analyser.fftSize = 1024;
+  analyser.fftSize = 4096;
+  gainNode.connect(analyser);
+  // frequencyBinCount is readonly and set to fftSize/2;
+  var timeDomain = new Uint8Array(analyser.frequencyBinCount);
+  var streaming = false;
+  var sampleRate = audioCtx.sampleRate;
+  var numSamples = analyser.frequencyBinCount;
+} else {
+  var analyser = {};
+  analyser.frequencyBinCount = 512;
+}
 
 if (navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia)) {
   navigator.getUserMedia({audio:true}, gotStream, function(error) {
