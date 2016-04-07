31408f7e787f37295c3fdf354c26bd35dbc042f7
Zachary Zimmerman <Oneman2feet@gmail.com>
2016-04-07 14:25:50 -0400

    use media streams

--- hilbert.js
+++ hilbert.js
@@ -1,10 +1,21 @@
 if (navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia)) {
-  console.log('we have user media');
-  navigator.getUserMedia({audio:true}, function(stream) {
-    console.log("we have an audio stream!");
-  }, function(error) {
+  navigator.getUserMedia({audio:true}, gotStream, function(error) {
     console.log("Capture error: ", error.code);
   });
 } else {
   console.log('you need a better browser');
 };
+
+function gotStream(stream) {
+  console.log('got stream');
+  // Create an AudioNode from the stream.
+  window.mediaStreamSource = audioContext.createMediaStreamSource(stream);
+  //for testing
+  var osc = audioContext.createOscillator();
+  osc.frequency.value = 200;
+  osc.start(0);
+  // switch these lines
+  window.mediaStreamSource.connect(gainNode);
+  // osc.connect(gainNode);
+  streaming = true;
+}
