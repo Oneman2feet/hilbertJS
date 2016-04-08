392e617e9fbbc6ca38d06976bc053aa0d8ae874e
Zachary Zimmerman <Oneman2feet@gmail.com>
2016-04-07 20:13:40 -0400

    try to draw some data

--- hilbert.js
+++ hilbert.js
@@ -17,9 +17,9 @@ if (AudioContext){
   var streaming = false;
   var sampleRate = audioCtx.sampleRate;
   var numSamples = analyser.frequencyBinCount;
+  analyser.getByteTimeDomainData(dataArray);
 } else {
-  var analyser = {};
-  analyser.frequencyBinCount = 512;
+  console.log("no audio context");
 }
 
 if (navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia)) {
@@ -43,3 +43,40 @@ function gotStream(stream) {
   // osc.connect(gainNode);
   streaming = true;
 }
+
+// draw an oscilloscope of the current audio source
+function draw() {
+  drawVisual = requestAnimationFrame(draw);
+
+  analyser.getByteTimeDomainData(dataArray);
+
+  canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+  canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+  canvasCtx.lineWidth = 2;
+  canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+
+  canvasCtx.beginPath();
+
+  var sliceWidth = WIDTH * 1.0 / bufferLength;
+  var x = 0;
+
+  for(var i = 0; i < bufferLength; i++) {
+
+    var v = dataArray[i] / 128.0;
+    var y = v * HEIGHT/2;
+
+    if(i === 0) {
+      canvasCtx.moveTo(x, y);
+    } else {
+      canvasCtx.lineTo(x, y);
+    }
+
+    x += sliceWidth;
+  }
+
+  canvasCtx.lineTo(canvas.width, canvas.height/2);
+  canvasCtx.stroke();
+};
+
+draw();
